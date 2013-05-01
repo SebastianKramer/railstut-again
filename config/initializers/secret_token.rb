a97071b90503f94448b82fc42c8746fe3e62791f
Sebastian Kramer <sebastiankramer@gmx.net>
2013-05-01 18:55:33 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratu2::Application.config.secret_token = '6d0f17210dea36a02ab7bc4c51c2025d1a8a1ea4a6978f8e90e9baf6efc66d53b47432df054f10910c30117a23dafd612fd44727e397388a2a1bc7a02d264d7d'
